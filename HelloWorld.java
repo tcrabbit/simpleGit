71f5bb431bf4500c62d304f801130c81092a2263
TelmoCosta <costatelmo@hotmail.com>
2018-01-19 12:22:34 +0100

    Make changes on HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -9,14 +9,12 @@ public class HelloWorld{
 	public static void doubleCalculate(){
 		int n=0;
 		System.out.println("Insert one number greater than 10\n");
-		while(n<10){
+		
 			Scanner sc = new Scanner(System.in);
 			n = sc.nextInt();
 			System.out.println("double =  " + n*2);
-			if(n<10){
-				System.out.println("the number must be greater than 10" );
-			}
-		}
-		
+			
 	}
+		
+	
 }
